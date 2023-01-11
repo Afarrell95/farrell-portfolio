603e2f4899299e177cc8d4d200edc91cbbeb5430
Allison01~ <farrella95@gmail.com>
2023-01-11 17:24:14 -0500

    first project deploy broke, added new project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Stellar by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -10,7 +6,7 @@
 		$body = $('body'),
 		$main = $('#main');
 
-	// Breakpoints.
+	
 		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
@@ -20,19 +16,19 @@
 			xxsmall:  [ null,      '360px'  ]
 		});
 
-	// Play initial animations on page load.
+	
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
 		});
 
-	// Nav.
+	
 		var $nav = $('#nav');
 
 		if ($nav.length > 0) {
 
-			// Shrink effect.
+			
 				$main
 					.scrollex({
 						mode: 'top',
@@ -44,7 +40,7 @@
 						},
 					});
 
-			// Links.
+			
 				var $nav_a = $nav.find('a');
 
 				$nav_a
@@ -56,16 +52,16 @@
 
 						var $this = $(this);
 
-						// External link? Bail.
+						
 							if ($this.attr('href').charAt(0) != '#')
 								return;
 
-						// Deactivate all links.
+						
 							$nav_a
 								.removeClass('active')
 								.removeClass('active-locked');
 
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+						
 							$this
 								.addClass('active')
 								.addClass('active-locked');
@@ -77,11 +73,11 @@
 							id = $this.attr('href'),
 							$section = $(id);
 
-						// No section for this link? Bail.
+						
 							if ($section.length < 1)
 								return;
 
-						// Scrollex.
+						
 							$section.scrollex({
 								mode: 'middle',
 								initialize: function() {
